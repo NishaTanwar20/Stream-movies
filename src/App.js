e33246f53581c47949319289f61cc8ac45860bea
Nisha Tanwar <103480687+NishaTanwar20@users.noreply.github.com>
2023-01-27 08:49:53 +0530

    code updated

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css"
+import HomePage from "./home/HomePage"
+import {Routes, Route } from "react-router-dom"
+import SinglePage from "./components/watch/SinglePage"
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+
+function App() {
+  return (
+    <>
+      
+        <Header />
+        <Routes>
+          <Route exact path='/' element={<HomePage />} />
+          <Route path='/singlepage/:id' element={<SinglePage />} exact />
+        </Routes>
+        <Footer />
+      
+    </>
+  )
+}
+
+export default App
